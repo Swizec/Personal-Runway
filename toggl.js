58248ee738b3c4bbc0528ec4ba76c0afdcb81e0d
Swizec Teller <swizec@swizec.com>
2011-12-26 19:45:56 +0100

    fetching time entries since Sept

--- toggl.js
+++ toggl.js
@@ -4,15 +4,21 @@ var https = require('https'),
 
 https.get({hostname: 'www.toggl.com',
            path: '/api/v6/time_entries.json?'+querystring.stringify({
-
+               start_date: (new Date('2011-09-01')).toISOString()
            }),
            auth: require('./secrets').toggl_api+':api_token'
           },
           function (res) {
               res.setEncoding('utf8');
-              res.on('data', function (chunk) {
-                  console.log("CHUNK: "+chunk);
-              });
+              var data = '';
+
+              res.on('data', function (chunk) { data += chunk; });
+              res.on('end', function () {parse_data(data);});
+              res.on('close', function () {parse_data(data);});
           }).on('error', function (e) {
               console.log("Got error:", e.message);
           });
+
+var parse_data = function (data) {
+    console.log(JSON.parse(data));
+};
