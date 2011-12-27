d539792ab8bde519735fca398e87672e6042d547
Swizec Teller <swizec@swizec.com>
2011-12-28 00:16:17 +0100

    plotting both datasets

--- modeling/graph.m
+++ modeling/graph.m
@@ -2,5 +2,9 @@
 toshl_Y = load('../dataset/toshl.txt');
 toggl_Y = load('../dataset/toggl.txt');
 
-axis([0, 366]);
-plot(toshl_Y, toggl_Y)
+figure
+plot(toshl_Y, "-b", toggl_Y, "-r")
+axis([0 366])
+ylabel('EUR');
+xlabel('Days');
+legend('Toshl', 'Toggl');
