c34048f83d21e44dc5a64b411816049da2ac1ca7
Swizec Teller <swizec@swizec.com>
2011-12-29 05:49:37 +0100

    can't quite figure out how to use the svm

--- implementation/svm_approach.hs
+++ implementation/svm_approach.hs
@@ -3,8 +3,15 @@ import SVM
 import IO
 import Data.List.Split
 
+-- can't quite figure this thing out
+
+learn :: [Double] -> SVMSolution
+learn vals =
+  let dataSet = DataSet points vals
+  let svm = LSSVM (KernelFunction radialKernelFunction) 0.5 0.5
+
 
 main = do
   lines <- readFile "../dataset/toshl.txt"
-  print (map (\x -> read x::Float)
-         (splitOn "\r\n" lines))
+  print (learn (map (\x -> read x::Double)
+                (splitOn "\r\n" lines)))
