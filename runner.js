822c208b9febcbdd1425fac4628bcaebba2ccde4
Swizec Teller <swizec@swizec.com>
2012-01-03 21:22:51 +0100

    getting the relevant part of the dataset

--- runner.js
+++ runner.js
@@ -6,6 +6,7 @@
 
 var toggl = require('./fetching/toggl'),
     toshl = require('./fetching/toshl'),
+    moment = require('moment'),
     EventEmitter = require('events').EventEmitter,
     _ = require('underscore');
 
@@ -16,20 +17,36 @@ var emitter = new EventEmitter();
 
 toggl.fetch_data(function (data) {
     console.log("toggl'd");
+    if (Data.toggl) return;
     Data.toggl = data;
     emitter.emit('fetched');
 });
 
 toshl.fetch_data(function (data) {
     console.log("toshl'd");
+    if (Data.toshl) return;
     Data.toshl = data;
     emitter.emit('fetched');
 });
 
 
 emitter.on('fetched', function () {
-    console.log("fetched");
     if (!(Data.toshl && Data.toggl)) return;
 
-//    console.log(_.size(Data.toshl), _.size(Data.toggl));
+    var last = moment(new Date('2011-12-01')).subtract("days", 14),
+        now = moment(new Date());
+
+    var _data = {},
+        keys = _.keys(Data.toshl).filter(
+            function (k) {
+                k = moment(k, "YYYY-DDD");
+                return k >= last && k <= now;
+            });
+
+    keys.map(function (k) {
+        _data[k] = {'+': Data.toggl[k],
+                    '-': Data.toshl[k]};
+    });
+
+    console.log(_data);
 });
