bf5401a21dd40ca2af783aedde7f687eacc91100
Swizec Teller <swizec@swizec.com>
2012-01-03 23:25:00 +0100

    storing data to mongo and creating points from which to continue on next run

--- runner.js
+++ runner.js
@@ -8,7 +8,9 @@ var toggl = require('./fetching/toggl'),
     toshl = require('./fetching/toshl'),
     moment = require('moment'),
     EventEmitter = require('events').EventEmitter,
-    _ = require('underscore');
+    _ = require('underscore'),
+    mongodb = require('mongodb'),
+    mongo = new mongodb.Db('personal-runway', new mongodb.Server('127.0.0.1', 27017, {}));
 
 var Data = {toshl: false,
             toggl: false};
@@ -29,24 +31,59 @@ toshl.fetch_data(function (data) {
     emitter.emit('fetched');
 });
 
+var last_known_state = function (callback) {
+    mongo.open(function (err) {
+        var meta = new mongodb.Collection(mongo, 'meta');
+        meta.find({}).sort({'time': -1}).limit(1).toArray(function(err, metas) {
+            var last = metas[0];
+            mongo.close();
+
+            callback(last);
+        });
+    });
+};
+
+var extract_data = function (last) {
+    var now = new Date(),
+        keys = _.keys(Data.toshl).filter(
+            function (k) {
+                k = moment(k, "YYYY-DDD");
+                return k >= last.time && k <= now;
+            });
+    return  keys.map(function (k) {
+        return {'day': moment(k, "YYYY-DDD")['_d'],
+                '+': Data.toggl[k],
+                '-': Data.toshl[k]};
+    });
+};
+
+var store_deltas = function (mongo, data) {
+    var deltas = new mongodb.Collection(mongo, 'deltas');
+    deltas.insert(data);
+};
+
+var replay_events = function (mongo, data, last) {
+    var new_amount = data.reduce(function (a, b) {
+        return a+b['+']-b['-'];
+    }, last.amount);
+
+    var metas = new mongodb.Collection(mongo, 'meta');
+    metas.insert({time: new Date(),
+                  amount: new_amount});
+};
 
 emitter.on('fetched', function () {
     if (!(Data.toshl && Data.toggl)) return;
 
-    var last = moment(new Date('2011-12-01')).subtract("days", 14),
-        now = moment(new Date());
+    last_known_state(function (last) {
 
-    var _data = {},
-        keys = _.keys(Data.toshl).filter(
-            function (k) {
-                k = moment(k, "YYYY-DDD");
-                return k >= last && k <= now;
-            });
+        var data = extract_data(last);
 
-    keys.map(function (k) {
-        _data[k] = {'+': Data.toggl[k],
-                    '-': Data.toshl[k]};
+        mongo.open(function (err) {
+            store_deltas(mongo, data);
+            replay_events(mongo, data, last);
+
+            mongo.close();
+        });
     });
-
-    console.log(_data);
 });
