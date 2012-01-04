0b7fb36444654f635bb43269034bee0c475c72e6
Swizec Teller <swizec@swizec.com>
2012-01-04 02:28:36 +0100

    simulating future from given amount of money

--- implementation/in_fortnight.hs
+++ implementation/in_fortnight.hs
@@ -4,26 +4,32 @@
 import Predict
 import Database.MongoDB
 import Control.Monad.Trans (liftIO)
+import System.Environment
 
 main = do
+  args <- getArgs
   pipe <- runIOE $ connect (host "127.0.0.1")
-  e <- access pipe master "personal-runway" run
+  e <- access pipe master "personal-runway" (run (read (args!!0)::Float))
   close pipe
   print e
 
-run = do
+run money = do
   -- left is latest
   deltas <- lastDeltas 30
 
-  let toggl = predict 14 $ reverse $ floatify (series "+" deltas)
-  let toshl = predict 14 $ reverse $ floatify (series "-" deltas)
+  let toggl = last14 $ predict 14 $ reverse $ floatify (series "+" deltas)
+  let toshl = last14 $ predict 14 $ reverse $ floatify (series "-" deltas)
 
-  -- last 14 are predictions
+  liftIO $ putStrLn $ show (simulate money toggl toshl)
 
 lastDeltas n = rest =<< find (select [] "deltas") {sort = ["day" =: -1]} {limit = n}
-
-printDocs title docs = liftIO $ putStrLn title >> mapM_ (print . exclude ["_id"]) docs
 
 series field docs = map (valueAt field) docs
 
 floatify xs = map (\x -> read x::Float) $ map show xs
+
+-- left is earliest
+last14 xs = reverse $ take 14 $ reverse xs
+
+simulate money toggl toshl =
+  foldl (\acc (plus, minus) -> acc+plus-minus) money $ zip toggl toshl
