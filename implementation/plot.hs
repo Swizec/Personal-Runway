f81daaaec47e42878060614fe08d6219a328491c
Swizec Teller <swizec@swizec.com>
2012-09-02 20:55:11 +0200

    graphing money fluctuation through time

--- implementation/plot.hs
+++ implementation/plot.hs
@@ -5,56 +5,32 @@ import Data.Time.Clock
 import Data.Time.LocalTime
 import Data.Colour.Names
 import Data.Colour
+import qualified Data.List as List
 import Data.Accessor
 import Graphics.Rendering.Chart
 
 import Database.MongoDB
 import Control.Monad.Trans (liftIO)
 
---import Prices
-
---date::Int -> Int -> Int -> LocalTime
---date dd mm yyyy = (LocalTime (fromGregorian (fromIntegral yyyy) mm dd) midnight)
---ok y = (y == 2005) || (y == 2006)
-
---chart = layout
---  where
-
---    price1 = plot_lines_style .> line_color ^= opaque blue
---           $ plot_lines_values ^= [[ (d, v) | (d,v,_) <- prices1]]
---           $ plot_lines_title ^= "price 1"
---           $ defaultPlotLines
-
---    price2 = plot_lines_style .> line_color ^= opaque green
---           $ plot_lines_values ^= [[ (d, v) | (d,v,_) <- prices2]]
---           $ plot_lines_title ^= "price 2"
---           $ defaultPlotLines
-
---    layout = layout1_title ^="Price History"
---           $ layout1_left_axis ^: laxis_override ^= axisGridHide
---           $ layout1_right_axis ^: laxis_override ^= axisGridHide
---           $ layout1_bottom_axis ^: laxis_override ^= axisGridHide
---           $ layout1_plots ^= [Left (toPlot price1),
---                               Right (toPlot price2)]
---           $ layout1_grid_last ^= False
---           $ defaultLayout1
-
-import Database.MongoDB
-import Control.Monad.Trans (liftIO)
-import System.Environment
-
-lastDeltas n = rest =<< find (select [] "deltas") {sort = ["day" Database.MongoDB.=: 1]} {limit = n}
-
-series field docs = map ((\x -> read x::Float) . show . (valueAt field))
-                         docs
+-- left is latest
+lastDeltas from n = rest =<< find (select ["day" Database.MongoDB.=:
+                                           ["$gte" Database.MongoDB.=: from]]
+                                   "deltas")
+                                  {sort = ["day" Database.MongoDB.=: 1]}
+known_point _ = rest =<< find (select [] "meta")
+                         {sort = ["time" Database.MongoDB.=: -1]}
+                         {skip = 10} {limit = 1}
 
 float::Label -> Document -> Double
 float field x =
   (read . show . (valueAt field)) x ::Double
 
+-- right is latest
 points::[Document] -> [(Maybe UTCTime, Double, Double)]
 points docs =
-  map (\doc -> (cast' $ valueAt "day" doc, float "+" doc, float "-" doc)) docs
+  map (\doc -> (cast' $ valueAt "day" doc,
+                float "+" doc,
+                float "-" doc)) $ docs
 
 date'::Int -> Int -> Int -> LocalTime
 date' dd mm yyyy = (LocalTime (fromGregorian (fromIntegral yyyy) mm dd) midnight)
@@ -63,26 +39,38 @@ date::Maybe UTCTime -> LocalTime
 date Nothing = date' 1 1 1970
 date (Just d) = utcToLocalTime utc d
 
-chart docs = layout
-  where
-    income = plot_lines_style .> line_color ^= opaque blue
-           $ plot_lines_values ^= [[ (date d, p) | (d, p, m) <- points docs]]
-           $ plot_lines_title ^= "Income"
-           $ defaultPlotLines
+simulate'::Double -> (Maybe UTCTime, Double, Double) -> (Double, (LocalTime, Double))
+simulate' money point =
+  let (d,p,m) = point
+  in (money+p-m, (date d, money+p-m))
 
-    expense = plot_lines_style .> line_color ^= opaque green
-           $ plot_lines_values ^= [[ (date d, m) | (d, p, m) <- points docs]]
-           $ plot_lines_title ^= "Expense"
+simulate::Maybe Double -> [Document] -> (Double, [(LocalTime, Double)])
+simulate base docs =
+  List.mapAccumL simulate' (double' base) (points docs)
+
+double'::Maybe Double -> Double
+double' Nothing = 0.0
+double' (Just x) = x
+
+chart points = layout
+  where
+    moneys = plot_lines_style .> line_color ^= opaque blue
+           $ plot_lines_values ^= [[ (d, v) | (d, v) <- points]]
+           $ plot_lines_title ^= "Income"
            $ defaultPlotLines
 
     layout = layout1_title ^="Money History"
            $ layout1_left_axis ^: laxis_override ^= axisGridHide
            $ layout1_right_axis ^: laxis_override ^= axisGridHide
            $ layout1_bottom_axis ^: laxis_override ^= axisGridHide
-           $ layout1_plots ^= [Left (toPlot income),
-                                     Right (toPlot expense)]
+           $ layout1_plots ^= [Left (toPlot moneys)]
+--                               Right (toPlot expense)]
            $ layout1_grid_last ^= False
            $ defaultLayout1
+
+known'::[Document] -> (Maybe UTCTime, Maybe Double)
+known' docs = (cast' $ (valueAt "time") (docs!!0),
+               cast' $ (valueAt "amount") (docs!!0))
 
 main = do
   pipe <- runIOE $ connect (host "127.0.0.1")
@@ -91,16 +79,14 @@ main = do
   print e
 
 run = do
-  deltas <- lastDeltas 30
+  known <- known_point 5
 
-  --let points = zip (series "+" deltas) (series "-" deltas)
+  let from = fst $ known' known
+  let money = snd $ known' known
 
-  liftIO $ renderableToPNGFile (toRenderable $ chart deltas) 640 480 "test.png"
+  deltas <- lastDeltas from 30
+
+  liftIO $ renderableToPNGFile (toRenderable $ chart $ snd $ simulate money deltas) 640 480 "test.png"
 
   liftIO $ putStrLn "done"
 
---  liftIO $ putStrLn $ show $ points deltas
-
-
-simulate money toggl toshl =
-  foldl (\acc (plus, minus) -> acc+plus-minus) money $ zip toggl toshl
