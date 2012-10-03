cc9ee2ffd7d7660ee590165c85ee5e8abe44c2ed
Swizec Teller <swizec@swizec.com>
2012-10-03 21:29:41 +0200

    a unit test for rates

---
+++ test/rates.js
@@ -0,0 +1,21 @@
+
+var chai = require('chai'),
+    mocha = require('mocha'),
+    should = chai.should();
+
+var rates = require('../fetching/rates');
+
+
+describe('Rate fetch', function () {
+    
+    it('should fetch rates', function (done) {
+        rates.fetch(function (err, data) {        
+
+            data.should.have.property('rates');
+            data.should.have.property('base').equal('USD');
+
+            done();
+        });
+    });
+
+});
