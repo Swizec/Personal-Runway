ec322c21045d2f4a4d592a36ac1019d857c44de3
Swizec Teller <swizec@swizec.com>
2014-11-14 01:58:33 +0100

    fixed a dirty problem in a dirty way

--- fetching/rates.js
+++ fetching/rates.js
@@ -10,6 +10,8 @@ exports.fetch = function (callback) {
                  query: {app_id: secrets.exchangerates}},
                 function (res) {
                     if (res.status === 200) {
+                        // ugly dirty fix for bad Toshl inputs
+                        res.body.rates.kn = res.body.rates.HRK;
                         callback(null, res.body);
                     }else{
                         callback(new Error("Failed to fetch rates"));
